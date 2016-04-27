f92309541218ec3cef7d4fa092449822d25d1e76
Kirill Averyanov <Kirillzzy@gmail.com>
2016-04-28 01:21:31 +0400

    Added files via upload

---
+++ try_everyday.cpp
@@ -0,0 +1,44 @@
+// try_everyday.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<new>
+#include<map>
+#include<time.h>
+#include<math.h>
+
+using namespace std;
+
+
+int binary_search(vector<int>a, int n) {
+	int first = 0;
+	int last = a.size() - 1;
+	while (true) {
+		int mid = (first + last) / 2;
+		if (n == a[mid])
+			return mid;
+		else if (n < a[mid]) {
+			last = mid - 1;
+		}
+		else if (n > a[mid]) {
+			first = mid + 1;
+		}
+		if (first > last)
+			return -1;
+	}
+}
+
+int main(){
+	srand(time(0));
+	vector<int>a;
+	for (int i = 0;i < 10;i++)
+		a.push_back(rand() % 20);
+	sort(a.begin(), a.end());
+	int k = rand() % 20;
+	cout << binary_search(a, k) << endl;
+	return 0;
+}
