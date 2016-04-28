5c22ad1131ec5b032a48557463f4b9dc156e082c
Kirill Averyanov <Kirillzzy@gmail.com>
2016-04-28 09:16:39 +0400

    Added files via upload

---
+++ try_everyday.cpp
@@ -0,0 +1,36 @@
+// try_everyday.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<new>
+#include<map>
+#include<time.h>
+#include<math.h>
+
+using namespace std;
+
+vector<int>bubble_sort(vector<int>what) {
+	for (int i = 0;i < what.size();i++) {
+		for (int j = what.size() - 1;j >= i;j--) {
+			if (what[j] < what[i]) {
+				int tmp = what[j];
+				what[j] = what[i];
+				what[i] = tmp;
+			}
+		}
+	}
+	return what;
+}
+int main(){
+	srand(time(0));
+	vector<int> a;
+	for (int i = 0;i < 10;i++) {
+		a.push_back(rand() % 20);
+	}
+	a = bubble_sort(a);
+	return 0;
+}
