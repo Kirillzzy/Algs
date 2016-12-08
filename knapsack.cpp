6af11d4af714e587782dac2bb43a9f84d3db551c
Kirill Averyanov <Kirillzzy@gmail.com>
2016-12-08 21:33:35 +0300

    new Knapsack

--- knapsack.cpp
+++ knapsack.cpp
@@ -1,34 +1,73 @@
-#include <bits/stdc++.h>                                      
-              
+#include <bits/stdc++.h>
+
 using namespace std;
 
+#define ll long long
 
-//knapsack
-int knapsack(vector<int>& wts, int W)
-{
-	size_t n = wts.size();
-	vector<bool> dp(W + 1);
-	dp[0] = 1;
-	for(int i=0;i<n;i++){
-		for(int j=W;j>=wts[i];j--){
-			dp[j] = max(dp[j],dp[j-wts[i]]);
+
+class Knapsack{
+public:
+	Knapsack(){};
+	vector<ll>ans;
+	ll n, m;
+	ll a[10005][100005];
+	ll wts[1005];
+	ll ps[1005];
+
+	void findAns(int k, int s){
+		if(a[k][s] == 0){
+			return;
+		}
+		if(a[k - 1][s] == a[k][s]){
+			findAns(k - 1, s);
+		}
+		else{
+			findAns(k - 1, s - wts[k]);
+			ans.push_back(k);
 		}
 	}
 
-	for(int i=W;i>=0;i--){
-		if(dp[i]==1){
-			return i;
+	void main(){
+		cin >> n >> m;
+		n++;
+		m++;
+		memset(a, 0, sizeof(a));
+		for(int i = 1; i < n; i++){
+			cin >> wts[i];
 		}
+		for(int i = 1; i < n; i++){
+			cin >> ps[i];
+		}
+
+		wts[0] = 0;
+		ps[0] = 0;
+
+		for(int i = 1; i < n; i++){
+			for(int j = 1; j < m; j++){
+				if(j >= wts[i]){
+					a[i][j] = max(a[i - 1][j], a[i - 1][j - wts[i]] + ps[i]);
+				}else{
+					a[i][j] = a[i - 1][j];
+				}
+			}
+		}
+
+		findAns(n - 1, m - 1);
+		cout << ans.size() << endl;
+		for(int i = 0; i < ans.size(); i++){
+			cout << ans[i] << " ";
+		}
+		cout << endl;
 	}
-}
+
+};
+
+
+
 
 int main(){
-	int s,n;
-	cin>>s>>n;
-	vector<int>wts(n);
-	for(int i=0;i<n;i++){
-		cin>>wts[i];
-	}
-	cout<<knapsack(wts,s)<<endl;
+	Knapsack *knp = new Knapsack();
+	knp -> main();
 	return 0;
 }
+
