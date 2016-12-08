b354a0d7301caf9959bd4a61e371717c48169b6a
Kirill Averyanov <Kirillzzy@gmail.com>
2016-12-08 20:10:53 +0300

    some fixes

--- longest_common_subsequence_2.cpp
+++ longest_common_subsequence_2.cpp
@@ -4,78 +4,61 @@ using namespace std;
 
 #define ll long long
 
-int p[500][500];
-
-
 int main(){
-	int n,m;
-	cin>>n>>m;
-	//
-	vector<int>a(n);
-	vector<int>b(m);
-	int y=0;
-	for(int i=0;i<n;i++){
-		cin>>a[i];
-	}
-	for(int i=0;i<m;i++){
-		cin>>b[i];
+	int n, m;
+
+	cin >> n;
+	int a[++n];
+
+	for(int i = 1; i < n; i++){
+		cin >> a[i];
 	}
 
+	cin >> m;
+	int b[++m];
 
-	for(int i=0;i<n;i++){
-		if(a[i]==b[0]){
-			y=1;
-		}
-		p[i][0] = y;
+	for(int i = 1; i < m; i++){
+		cin >> b[i];
 	}
-	y=0;
 
-	for(int i=0;i<m;i++){
-		if(b[i]==a[0]){
-			y=1;
-		}
-		p[0][i] = y;
-	}
-	for(int i=1;i<n;i++){
-		for(int j=1;j<m;j++){
-			if(a[i]==b[j]){				
-				p[i][j]=p[i-1][j-1]+1;
+	int dp[n][m];
+	memset(dp, 0, sizeof(dp));
+	
+	a[0] = -1;
+	b[0] = -2;
+
+	for(int i = 1; i < n; i++){
+		for(int j = 1; j < m; j++){
+			if(a[i] == b[j]){
+				dp[i][j] = dp[i - 1][j - 1] + 1;
 			}else{
-				int u = max(p[i-1][j],p[i][j-1]);
-				p[i][j]=u;
+				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
 			}
 		}
 	}
-	cout<<p[n-1][m-1]<<endl;
-    vector<int>s;
+	cout << dp[n - 1][m - 1] << endl;
 
-    int i = 0;
-
-   	int j = 0;
-
-    while (i < n && j < m)
-
-    {
-
-	if (a[i]==b[j])
-
-	{
-
-	    s.push_back(a[i]);
-
-	    i++; j++;
+	vector<int>ans;
+	int i = n - 1;
+	int j = m - 1;
+	while(i > 0 && j > 0){
+		if(a[i] == b[j]){
+			ans.push_back(a[i]);
+			i--;
+			j--;
+		}
+		else if(dp[i - 1][j] >= dp[i][j - 1]){
+			i--;
+		}else{
+			j--;
+		}
 
 	}
-
-	else if (p[i+1][j] >= p[i][j+1]) i++;
-
-	else j++;
-
-    }
-
-    for(int i=0;i<s.size();i++){
-    	cout<<s[i]<<" ";
-    }
-	cout<<endl;
+	reverse(ans.begin(), ans.end());
+	for(int i = 0; i < ans.size(); i++){
+		cout << ans[i] << " ";
+	}
+	cout << endl;
 	return 0;
 }
+
