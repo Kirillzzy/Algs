a434e425daa3f67dbecc66a32015d2d9c6dd6ab5
Kirill Averyanov <Kirillzzy@gmail.com>
2016-12-08 22:16:28 +0300

    Spaces

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -8,61 +8,61 @@ typedef long long ll;
 
 class foo{
 public:
-	foo(int n):n(n){
+	foo(int n): n(n){
 		items.resize(n);
 	}
 
 	foo(){
-		n=10;
+		n = 10;
 		items.resize(n);
 	}
 
 	void sort(){
-		qsort(0,n-1);
+		qsort(0, n - 1);
 	}
 
 	void randomize(){
-		for(int i=0;i<n;i++){
-			items[i] = rand()%n;
+		for(int i = 0; i < n; i++){
+			items[i] = rand() % n;
 		}
 	}
 
 	void printItems(){
-		for(int i=0;i<n;i++){
-			cout<<items[i]<<" ";
+		for(int i = 0; i < n; i++){
+			cout << items[i] << " ";
 		}
-		cout<<endl;
+		cout << endl;
 	}
 
 	~foo(){}
 
 private:
-	vector<int>items;
+	vector<int> items;
 	int n;
-	void qsort(int l,int r){
-		int i=l;
-		int j=r;
-		int x = items[(l+r)/2];
-		while(i<=j){
-			while(items[i]<x){
+	void qsort(int l, int r){
+		int i = l;
+		int j = r;
+		int x = items[(l + r) / 2];
+		while(i <= j){
+			while(items[i] < x){
 				i++;
 			}
-			while(items[j]>x){
+			while(items[j] > x){
 				j--;
 			}
-			if(i<=j){
+			if(i <= j){
 				int tmp = items[i];
-				items[i]=items[j];
-				items[j]=tmp;
+				items[i] = items[j];
+				items[j] = tmp;
 				i++;
 				j--;
 			}
 		}
-		if(l<j){
-			qsort(l,j);
+		if(l < j){
+			qsort(l, j);
 		}
-		if(i<r){
-			qsort(i,r);
+		if(i < r){
+			qsort(i, r);
 		}
 	}
 
@@ -71,7 +71,7 @@ private:
 
 int main(){
 	int n;
-	cin>>n;
+	cin >> n;
 	foo nums(n);
 	nums.randomize();
 	nums.printItems();
