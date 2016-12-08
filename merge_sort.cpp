c2794b7f640c6b14bf222eb0a1ba31ac3b9f70d5
Kirill Averyanov <Kirillzzy@gmail.com>
2016-12-08 22:06:58 +0300

    Spaces added

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -8,13 +8,13 @@ int a[100000000];
 int t[100000000];
 
 
-void merge(int a[],int l,int r){
-	int m = (l+r)/2;
+void merge(int a[], int l, int r){
+	int m = (l + r) / 2;
 	int n = m;
 	int p = l;
 	int p1 = l;
-	while(l<n&&m<r){
-		if(a[l]<a[m]){
+	while(l < n && m < r){
+		if(a[l] < a[m]){
 			t[p] = a[l];
 			l++;
 		}else{
@@ -23,42 +23,42 @@ void merge(int a[],int l,int r){
 		}
 		p++;
 	}
-	while(l<n){
+	while(l < n){
 		t[p] = a[l];
 		l++;
 		p++;
 	}
-	while(m<r){
+	while(m < r){
 		t[p] = a[m];
 		m++;
 		p++;
 	}
-	for(int i=p1;i<r;i++){
+	for(int i = p1; i < r; i++){
 		a[i] = t[i];
 	}
 }
 
-void merge_sort(int a[],int first,int last){
-	if(last-first==1){
+void merge_sort(int a[], int first, int last){
+	if(last - first == 1){
 		return;
 	}
-	merge_sort(a,first,(first+last)/2);
-	merge_sort(a,(first+last)/2,last);
-	merge(a,first,last);
-	for(int i=0;i<nums;i++){
-		cout<<a[i]<<" ";
+	merge_sort(a, first, (first + last) / 2);
+	merge_sort(a, (first + last) / 2, last);
+	merge(a, first, last);
+	for(int i = 0; i < nums; i++){
+		cout << a[i] << " " ;
 	}
-	cout<<endl;
+	cout << endl;
 }
 
 
 
 int main()
 {
-	cin>>nums;
-	for(int i=0;i<nums;i++){
-		cin>>a[i];
+	cin >> nums;
+	for(int i = 0; i < nums; i++){
+		cin >> a[i];
 	}
- 	merge_sort(a,0,nums);
+ 	merge_sort(a, 0, nums);
 	return 0;
 }   
